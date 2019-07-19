defe763e590b4afdd44108b35e5da5ab6cec0cfb
owen <649073762@qq.com>
2019-07-19 15:01:51 +0800

    first commit

---
+++ src/components/frist.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <button @click="add">+</button>
+    <h1>{{ count }}</h1>
+    <span></span>
+    <button @click="deep">-</button>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'count',
+   props: {
+    msg: String
+  },
+  data(){
+      return {
+          count :0
+      }
+  },
+  methods:{
+      add(){
+         this.count++
+      },
+      deep(){
+         this.count--
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
